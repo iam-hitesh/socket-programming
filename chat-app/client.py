56375e1ea68b0557d0158203235ac6759e9f2f2c
Hitesh Yadav <hhiteshyadav@gmail.com>
2019-11-30 16:35:45 +0530

    Update client.py

--- chat-app/client.py
+++ chat-app/client.py
@@ -1,72 +1,55 @@
 import socket
 import select
+import errno
+import sys
 
-HEADER_LENGTH = 10
+HEADER_LENGTH  = 10
+
 IP = "127.0.0.1"
 PORT = 1234
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+self_username = input("Username: ")
 
-# For Reconnecting Purpose
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((IP, PORT))
+client_socket.setblocking(False)
 
-server_socket.bind((IP, PORT))
-server_socket.listen(5)
+self_username = self_username.encode("utf-8")
+username_header = f"{len(self_username) :< {HEADER_LENGTH}}".encode("utf-8")
 
-# List of sockets
-sockets_list = [server_socket]
-clients = {}
-
-
-def receive_msg(client_socket):
-    try:
-        message_header = client_socket.recv(HEADER_LENGTH)
-
-        if not len(message_header):
-            return False
-
-        message_length = int(message_header.decode("utf-8"))
-
-        return {
-            "header": message_header,
-            "data": client_socket.recv(message_length)
-        }
-    except:
-        return False
-
+client_socket.send(username_header + self_username)
 
 while True:
-    read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
+    message = input(f"{self_username} >>> ")
 
-    for notified_socket in read_sockets:
-        if notified_socket == server_socket:
-            client_socket, client_address = server_socket.accept()
-            user = receive_msg(client_socket)
+    if message:
+        message = message.encode("utf-8")
+        message_header = f"{len(message):<{HEADER_LENGTH}}".encode("utf-8")
+        client_socket.send(message_header + message)
 
-            if user is False:
-                continue
+    try:
+        while True:
+            # For receiving messages
+            username_header = client_socket.recv(HEADER_LENGTH)
+            if not len(username_header):
+                print("Server Gone Away")
+                sys.exit()
 
-            sockets_list.append(client_socket)
-            clients[client_socket] = user
+            username_length = int(username_header.decode("utf-8").strip())
+            username = client_socket.recv(username_length).decode("utf-8")
 
-            print("Accepted New Connection")
+            message_header = client_socket.recv(HEADER_LENGTH)
+            message_length = int(message_header.decode("utf-8").strip())
+            message = client_socket.recv(message_length).decode("utf-8")
 
-        else:
-            message = receive_msg(notified_socket)
+            print(f"{username} <<< {message}")
 
-            if message is False:
-                print("Connection Closed")
-                sockets_list.remove(notified_socket)
-                del clients[notified_socket]
-                continue
+    except IOError as e:
+        if e.errno != errno.EAGAIN or e.errno != errno.EWOULDBLOCK:
+            print("Reading Error", str(e))
+            sys.exit()
+        continue
 
-            user = clients[notified_socket]
-            print("Received Message from")
-
-            for client_socket in clients:
-                if client_socket != notified_socket:
-                    client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
-
-    for notified_socket in exception_sockets:
-        sockets_list.remove(notified_socket)
-        del clients[notified_socket]
+    except Exception as e:
+        print("General Error")
+        pass
